347f269e21a6cf690961b6a18332545727d4fe97
unknown <kanschje@DEESS1NB0350.de.root.net>
2015-10-02 16:49:09 +0200

    Yeoman generation

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hubApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hubApp
+ */
+angular.module('hubApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hubApp
+ */
+angular.module('hubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
